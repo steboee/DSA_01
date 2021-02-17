fb92af142fb03759db4c56bfeac69dff3017b836
Ctibor Kovalčík <ckkoval33@gmail.com>
2021-02-17 14:59:06 +0100

    NOW START

---
+++ DSA_01/Source.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void* memory_alloc(unsigned int size) {
+
+}
+int memory_free(void* valid_ptr) {
+
+}
+int memory_check(void* ptr) {
+
+}
+void memory_init(void* ptr, unsigned int size) {
+
+}
+
+
+int main() {
+}
